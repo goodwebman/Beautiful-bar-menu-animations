6233fbe29c79d1097e56a614089a25010f07267b
goodwebman <gitweb2danya@mail.ru>
2026-04-16 12:18:34 +0600

    fix: hero section mojito mobile scroll position

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
 			.to('.arrow', { y: 100 }, 0)
 
 		const startValue = isMobile ? 'top 50%' : 'center 60%'
-		const endValue = isMobile ? '120% top' : 'bottom top'
+		const endValue = isMobile ? '272% top' : 'bottom top'
 
 		const tl = gsap.timeline({
 			scrollTrigger: {
